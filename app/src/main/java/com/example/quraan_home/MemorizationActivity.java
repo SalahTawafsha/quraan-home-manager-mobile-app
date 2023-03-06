084b96770ec33d8ee9bed28f25fb09a9c8537aab
Salah Tawafsha <93351227+SalahTawafsha@users.noreply.github.com>
2023-03-06 17:23:12 +0200

    V1.9

--- app/src/main/java/com/example/quraan_home/MemorizationActivity.java
+++ app/src/main/java/com/example/quraan_home/MemorizationActivity.java
@@ -3,7 +3,6 @@ package com.example.quraan_home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -17,7 +16,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 import entities.Student;
 
