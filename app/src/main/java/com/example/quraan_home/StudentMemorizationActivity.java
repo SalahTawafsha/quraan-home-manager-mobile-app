a55fd79fbb923f7b289de833fc3387b190fa3e2d
Salah Tawafsha <93351227+SalahTawafsha@users.noreply.github.com>
2023-03-10 23:10:24 +0200

    V2.00

--- app/src/main/java/com/example/quraan_home/StudentMemorizationActivity.java
+++ app/src/main/java/com/example/quraan_home/StudentMemorizationActivity.java
@@ -3,6 +3,7 @@ package com.example.quraan_home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -21,12 +23,18 @@ import entities.Student;
 
 public class StudentMemorizationActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_memorization);
 
+        ListView listView = findViewById(R.id.parts);
+
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            Intent intent = new Intent(this, StudentMemorizationPartsActivity.class);
+            intent.putExtra("part", i);
+            startActivity(intent);
+        });
     }
 
     public void finish(View view) {
--- app/src/main/java/com/example/quraan_home/StudentMemorizationPartsActivity.java
+++ app/src/main/java/com/example/quraan_home/StudentMemorizationPartsActivity.java
@@ -35,10 +35,104 @@ public class StudentMemorizationPartsActivity extends AppCompatActivity {
         sharedPref = getSharedPreferences(
                 getString(R.string.login)
                 , Context.MODE_PRIVATE);
+        
+        String[] lines;
 
-        String s = getString(R.string.surah);
+        int x = getIntent().getIntExtra("part", 1) + 1;
+        switch (x) {
+            case 1:
+                lines = getResources().getStringArray(R.array.part1);
+                break;
+            case 2:
+                lines = getResources().getStringArray(R.array.part2);
+                break;
+            case 3:
+                lines = getResources().getStringArray(R.array.part3);
+                break;
+            case 4:
+                lines = getResources().getStringArray(R.array.part4);
+                break;
+            case 5:
+                lines = getResources().getStringArray(R.array.part5);
+                break;
+            case 6:
+                lines = getResources().getStringArray(R.array.part6);
+                break;
+            case 7:
+                lines = getResources().getStringArray(R.array.part7);
+                break;
+            case 8:
+                lines = getResources().getStringArray(R.array.part8);
+                break;
+            case 9:
+                lines = getResources().getStringArray(R.array.part9);
+                break;
+            case 10:
+                lines = getResources().getStringArray(R.array.part10);
+                break;
+            case 11:
+                lines = getResources().getStringArray(R.array.part11);
+                break;
+            case 12:
+                lines = getResources().getStringArray(R.array.part12);
+                break;
+            case 13:
+                lines = getResources().getStringArray(R.array.part13);
+                break;
+            case 14:
+                lines = getResources().getStringArray(R.array.part14);
+                break;
+            case 15:
+                lines = getResources().getStringArray(R.array.part15);
+                break;
+            case 16:
+                lines = getResources().getStringArray(R.array.part16);
+                break;
+            case 17:
+                lines = getResources().getStringArray(R.array.part17);
+                break;
+            case 18:
+                lines = getResources().getStringArray(R.array.part18);
+                break;
+            case 19:
+                lines = getResources().getStringArray(R.array.part19);
+                break;
+            case 20:
+                lines = getResources().getStringArray(R.array.part20);
+                break;
+            case 21:
+                lines = getResources().getStringArray(R.array.part21);
+                break;
+            case 22:
+                lines = getResources().getStringArray(R.array.part22);
+                break;
+            case 23:
+                lines = getResources().getStringArray(R.array.part23);
+                break;
+            case 24:
+                lines = getResources().getStringArray(R.array.part24);
+                break;
+            case 25:
+                lines = getResources().getStringArray(R.array.part25);
+                break;
+            case 26:
+                lines = getResources().getStringArray(R.array.part26);
+                break;
+            case 27:
+                lines = getResources().getStringArray(R.array.part27);
+                break;
+            case 28:
+                lines = getResources().getStringArray(R.array.part28);
+                break;
+            case 29:
+                lines = getResources().getStringArray(R.array.part29);
+                break;
+            default:
+                lines = getResources().getStringArray(R.array.part30);
+                break;
 
-        String[] lines = s.split("[,]");
+
+        }
 
         database.child("student").child(sharedPref.getString("currStudent", ""))
                 .get().addOnCompleteListener(task -> {
